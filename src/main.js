8d090c7ce9b284728ee8ffb5c709077f25814929
German Mas <german.e.mas@gmail.com>
2022-02-28 15:28:19 +0000

    Version 2 - Scene

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 // List of exports the GZ3D library will have.
 import { FuelServer } from './fuel-server';
+import { Ogre2Json } from './loaders/Ogre2Json';
+import { Scene } from './scene';
 import { SdfParser } from './sdf-parser';
+import { Shaders } from './shaders';
 
 export {
   FuelServer,
+  Ogre2Json,
+  Scene,
   SdfParser,
+  Shaders,
 };
