d2e22d19e4b8a7db24b0b61bdd9ff0293e91bc49
Nate Koenig <nate@openrobotics.org>
2022-06-14 05:42:26 -0700

    clean build
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- rollup.config.js
+++ rollup.config.js
@@ -5,17 +5,29 @@ import babel from '@rollup/plugin-babel';
 
 export default {
   input: 'tsc-out/src/gzweb.js',
+  external: [
+    'eventemitter2',
+    'protobufjs',
+  ],
   output: [
     {
       file: 'dist/gzweb.js',
       format: 'umd',
       name: 'gzweb',
+      globals: {
+        eventemitter2: 'eventemitter2',
+        protobufjs: 'protobufjs',
+      }
     },
     {
       file: 'dist/gzweb.min.js',
       format: 'umd',
       name: 'gzweb',
-      plugins: [terser()]
+      plugins: [terser()],
+      globals: {
+        eventemitter2: 'eventemitter2',
+        protobufjs: 'protobufjs',
+      }
     }
   ],
 
